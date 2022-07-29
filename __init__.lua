1740867761c88a06942c4bd17aa0f41bda52c9e0
Chenterito <64875738+Chenterito@users.noreply.github.com>
2022-07-29 17:10:36 -0500

    Add files via upload

---
+++ __init__.lua
@@ -0,0 +1,46 @@
+-- this callback function will run EVERY TIME a new player has connected
+level:onnotifyonce("game_ended", function()
+
+    local gametypewin = getgametypewin()
+    local mapwin = getmapwin()
+    --player.spawns = 0 -- we setup a new variable to keep count of spawns on the player entity
+    game:executecommand('set sv_maprotationcurrent "gametype ' .. gametypewin .. ' map ' .. mapwin .. '"')
+    print('set sv_maprotationcurrent "gametype ' .. gametypewin .. ' map ' .. mapwin .. '"')
+    -- this callback function will run EVERY TIME the player has spawned
+    
+    
+end)
+
+function getgametypewin()
+    
+    local gametypesa = {}
+    table.insert(gametypesa, "war")
+    table.insert(gametypesa, "dom")
+    table.insert(gametypesa, "hp")
+    --table.insert(gametypesa, "sd")
+    
+    return gametypesa[math.random(#gametypesa)]
+end
+
+function getmapwin()
+
+    local mapsa = {}
+    table.insert(mapsa, "mp_convoy")
+    table.insert(mapsa, "mp_showdown")
+    table.insert(mapsa, "mp_bog")
+    table.insert(mapsa, "mp_crash")
+    table.insert(mapsa, "mp_crossfire")
+    table.insert(mapsa, "mp_citystreets")
+    table.insert(mapsa, "mp_shipment")
+    table.insert(mapsa, "mp_vacant")
+    table.insert(mapsa, "mp_broadcast")
+    table.insert(mapsa, "mp_bloc")
+    table.insert(mapsa, "mp_killhouse")
+    table.insert(mapsa, "mp_strike")
+    table.insert(mapsa, "mp_crash_snow")
+    table.insert(mapsa, "mp_countdown")
+    table.insert(mapsa, "mp_bog_summer")
+
+    return mapsa[math.random(#mapsa)]
+
+end
